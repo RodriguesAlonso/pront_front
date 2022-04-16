e9dadea157ebfaf94f0ecad5a5e0335f78d97961
Alonso <joaoalonso42@gmail.com>
2022-04-16 17:42:08 -0300

    mais um teste

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "Hello Medium"
-
 @app.route('/')
 def index():
         return render_template('index.html')
