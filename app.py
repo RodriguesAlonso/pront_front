4c86fa2e48a3851d18c5701c2645769240407158
Alonso <joaoalonso42@gmail.com>
2022-04-16 20:54:06 -0300

    heroku fix

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 from ext import views
 
 app = Flask(__name__)
 views.init_app(app)
 
-if __name__ == '__main__':
-    app.run(port=5000)
