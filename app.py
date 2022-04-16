5bcf9778e1918dcb3274b5093ec7b97ddd45e3b3
Alonso <joaoalonso42@gmail.com>
2022-04-16 17:28:41 -0300

    basic

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from ext import views
 
 app = Flask(__name__)
-views.init_app(app)
-app.run()
-    
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
