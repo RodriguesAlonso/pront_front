21c317586a5921741e6ece69a12cb587f4afafb9
Alonso <joaoalonso42@gmail.com>
2022-04-16 20:40:23 -0300

    first commit database

---
+++ ext/database.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+def init_database(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cowqxsowfwmzxg:abebd323848c44cba1fa2c7c508e4771bed17524adc8f629ac34ceadca8d0ec3@ec2-34-197-84-74.compute-1.amazonaws.com:5432/dauskq533vma6p'
+
+    db = SQLAlchemy(app)
---
+++ models.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from ext.database import db
+from sqlalchemy_serializer import SerializerMixin
+
+
+class Usuario(db.Model, SerializerMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	nome = db.Column(db.String(140), nuLLable=False)
+    date_create = db.Column(db.DateTime, default=datetime.utcnow)
+	#dataNacimento = db.Column(db.DateTime)
+	#login = db.Column(db.String(20), unique=True)
+	#senha = db.Column(db.String(20))
+	#paciente_id = db.Column(db.Integer, db.ForeignKey('paciente_id'))
+    def __repr__(self):
+        return '<Nome %r>' % self.id
+
+class Medico(db.Model, SerializerMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	crm = db.Column(db.String(10), unique=True)
+	nome = db.Column(db.String(140), unique=True)
+	#id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+	
+class Pacientes(db.Model, SerializerMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	CartSus = db.Column(db.String(20), unique=True)
+	#histFam = db.Column(db.String(300))
+	histPac = db.Column(db.Text())
+	#nome = db.relationship('Usuario', backref='paciente', lazy=True)
