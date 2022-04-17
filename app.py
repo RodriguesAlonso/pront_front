0a98d0a506f8c9d419a0a9505f6c443227b5368e
Alonso <joaoalonso42@gmail.com>
2022-04-16 21:13:08 -0300

    import database

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from ext import views
+from ext import database
 #from ext import database
 
 app = Flask(__name__)
 views.init_app(app)
+database.init_app(app)
 
--- ext/database.py
+++ ext/database.py
@@ -4,6 +4,6 @@ from datetime import datetime
 '''
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cowqxsowfwmzxg:abebd323848c44cba1fa2c7c508e4771bed17524adc8f629ac34ceadca8d0ec3@ec2-34-197-84-74.compute-1.amazonaws.com:5432/dauskq533vma6p'
 
+'''
 def init_app(app):
     db = SQLAlchemy(app)
-'''
