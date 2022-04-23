3e287f756b2e1bc6c3d97a6696d3205a42474249
Alonso <joaoalonso42@gmail.com>
2022-04-23 17:49:17 -0300

    c

--- app.py
+++ app.py
@@ -5,12 +5,9 @@ from pront_front.ext import config
 from pront_front.ext import database
 
 
-#def create_app():
-
-app = Flask(__name__)
-config.init_app(app)
-database.init_app(app)
-site.init_app(app)
-    #return app
-if __name__ == '__main__':
-    app.run(port=5000)
+def create_app():
+    app = Flask(__name__)
+    config.init_app(app)
+    database.init_app(app)
+    site.init_app(app)
+    return app
